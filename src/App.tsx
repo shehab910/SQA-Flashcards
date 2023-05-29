4695b4e5102d7a1b30527327c1d68d3b9db5d01b
Shehab Ihab <shehabihab@hotmail.com>
2023-05-29 05:37:07 +0300

    UI for mobile only

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,54 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [isAnswerShown, setIsAnswerShown] = useState(false);
+	const [isHintShown, setIsHintShown] = useState(false);
 	const questionHeadding = "Replace with the scientific term";
 	const question =
 		"Problems identified when team members do not follow applicable standards, recorded processes, or procedures.";
+	const answer = "Noncompliance";
+	const hint = "applicable standards, recorded processes, or procedures.";
+	const slideLink = "https://www.google.com";
 	return (
 		<>
-			<h2>{questionHeadding}</h2>
-			<h1>{question}</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
+			<div id="title">
+				<h1>احفظ يا طالب</h1>
 			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+			<div className="main">
+				<h3>{questionHeadding}:</h3>
+				<h2>{question}</h2>
+				<p className={`hint ${!(isHintShown || isAnswerShown) && "blur"}`}>
+					Hint: {hint}
+				</p>
+				<p className={`answer ${!isAnswerShown ? "blur" : ""}`}>{answer}</p>
+			</div>
+			<div className="btn-grp controls ">
+				<button
+					id="show-hint"
+					disabled={isHintShown || isAnswerShown}
+					onClick={() => setIsHintShown(true)}
+				>
+					هممم
+				</button>
+				{!isAnswerShown && (
+					<button
+						onClick={() => setIsAnswerShown(true)}
+						className="btn-contained"
+					>
+						الاجابة
+					</button>
+				)}
+				{isAnswerShown && (
+					<a href={slideLink} target="#blank">
+						<button className="btn-contained">وريني المحاضرة</button>
+					</a>
+				)}
+
+				<div className="nav-btn-grp">
+					<button>ثواني كدا</button>
+					<button className="btn-outline">البعده</button>
+				</div>
+			</div>
 		</>
 	);
 }
